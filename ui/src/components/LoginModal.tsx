13ad6b82e034851db9343179e679f80ce18b2201
Swopper050 <bram@witoos.nl>
2024-09-25 08:11:16 +0200

    switching pc's again

--- ui/src/components/LoginModal.tsx
+++ ui/src/components/LoginModal.tsx
@@ -1,6 +1,8 @@
 import {JSXElement, createSignal, Show} from "solid-js";
 import { clsx } from 'clsx';
 
+import { EmailIcon } from "./icons/Email";
+import { PasswordIcon } from "./icons/Password";
 import { post } from "../fetch";
 
 
@@ -37,41 +39,23 @@ export function LoginModal(): JSXElement {
           <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
         </form>
         <label class="input input-bordered flex items-center gap-2 mb-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            class="h-4 w-4 opacity-70">
-            <path
-              d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-            <path
-              d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-          </svg>
+          <EmailIcon />
           <input
             type="text"
             class="grow"
             placeholder="your@email.com"
             value={email()} 
-            onChange={(e) => setEmail(e.target.value === "" ? null : e.target.value)}
+            onInput={(e) => setEmail(e.target.value === "" ? null : e.target.value)}
           />
         </label>
         <label class="input input-bordered flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            class="h-4 w-4 opacity-70">
-            <path
-              fill-rule="evenodd"
-              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-              clip-rule="evenodd" />
-          </svg>
+          <PasswordIcon />
           <input
             type="password"
             class="grow"
             placeholder="Your password"
             value={password()}
-            onChange={(e) => setPassword(e.target.value === "" ? null : e.target.value)}
+            onInput={(e) => setPassword(e.target.value === "" ? null : e.target.value)}
           />
         </label>
         <Show when={errorMsg() !== null}>
@@ -89,7 +73,6 @@ export function LoginModal(): JSXElement {
             </Show>
             Login
           </button>
-          <button class={clsx("btn", "btn-primary", "btn-outline", submitting() && "btn-disabled")}>Register</button>
         </div>
       </div>
     </dialog>
--- ui/src/components/RegisterModal.tsx
+++ ui/src/components/RegisterModal.tsx
@@ -1,17 +1,102 @@
-import {JSXElement} from "solid-js";
+import {JSXElement, createSignal, Show} from "solid-js";
+import { clsx } from 'clsx';
+
+import { EmailIcon } from "./icons/Email";
+import { PasswordIcon } from "./icons/Password";
+import { post } from "../fetch";
 
 
 export function RegisterModal(): JSXElement {
+  const [email, setEmail] = createSignal<string | null>(null);
+  const [password, setPassword] = createSignal<string | null>(null);
+  const [checkPassword, setCheckPassword] = createSignal<string | null>(null);
+  const [errorMsg, setErrorMsg] = createSignal<string | null>(null);
+
+  const [submitting, setSubmitting] = createSignal(false);
+
+  const handleLogin = async () => {
+    setSubmitting(true)
+
+    const response = await post(
+      '/api/register',
+      JSON.stringify({ email: email(), password: password() })
+    )
+
+    if (!response.ok) {
+      setErrorMsg((await response.json()).error_message)
+      setSubmitting(false)
+    } else {
+      const user = new User(await response.json())
+      setUser(user)
+      localStorage.setItem('access_token', user.accessToken ?? '')
+    }
+  }
+
   return (
     <dialog id="register_modal" class="modal">
       <div class="modal-box">
-        <h3 class="text-lg font-bold">Hello!</h3>
-        <p class="py-2">This is the register modal.</p>
-        <p class="py-4">Press ESC key or click the button below to close</p>
-        <div class="modal-action">
-          <form method="dialog">
-            <button class="btn">Close</button>
-          </form>
+        <h3 class="flex justify-center text-lg font-bold mb-6">
+          Register
+        </h3>
+
+        <form method="dialog">
+          <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+
+        <label class="input input-bordered flex items-center gap-2 mb-3">
+          <EmailIcon />
+          <input
+            type="text"
+            class="grow"
+            placeholder="your@email.com"
+            value={email()} 
+            onInput={(e) => setEmail(e.target.value === "" ? null : e.target.value)}
+          />
+        </label>
+
+        <label class="input input-bordered flex items-center gap-2 mb-3">
+          <PasswordIcon />
+          <input
+            type="password"
+            class="grow"
+            placeholder="Your password"
+            value={password()}
+            onInput={(e) => setPassword(e.target.value === "" ? null : e.target.value)}
+          />
+        </label>
+
+        <label class={clsx("input", "input-bordered", "flex", "items-center", "gap-2", (password() !== checkPassword()) && "input-error")}>
+          <PasswordIcon />
+          <input
+            type="password"
+            class="grow"
+            placeholder="Confirm password"
+            value={checkPassword()}
+            onInput={(e) => setCheckPassword(e.target.value === "" ? null : e.target.value)}
+          />
+        </label>
+        <Show when={password() !== checkPassword()}>
+          <div class="label">
+            <span class="label-text-alt text-error">Passwords do not match</span>
+          </div>
+        </Show>
+
+        <Show when={errorMsg() !== null}>
+          <div role="alert" class="alert alert-error my-6">
+            <span>{errorMsg()}</span>
+          </div>
+        </Show>
+
+        <div class="modal-action flex justify-center">
+          <button
+            class={clsx("btn", "btn-primary", submitting() && "btn-disabled")}
+            onClick={handleLogin}
+          >
+            <Show when={submitting()}>
+              <span class="loading loading-spinner"></span>
+            </Show>
+            Register
+          </button>
         </div>
       </div>
     </dialog>
