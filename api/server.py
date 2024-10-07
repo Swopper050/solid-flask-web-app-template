247c7f3fe4f0f1b04812b79865efc4f2fbccc70f
Bram <b.dewit@applyai.nl>
2024-10-07 11:01:57 +0200

    Fix gunicorn entrypoint (#25)
    Co-authored-by: Swopper050 <bram@witoos.nl>

--- api/server.py
+++ api/server.py
@@ -3,8 +3,6 @@ from flask.helpers import get_debug_flag
 from app.app import create_app
 from app.config import DevConfig, ProdConfig
 
-if __name__ == "__main__":
-    CONFIG = DevConfig() if get_debug_flag() else ProdConfig()
+CONFIG = DevConfig() if get_debug_flag() else ProdConfig()
 
-    app = create_app(config_object=CONFIG)
-    app.run()
+app = create_app(config_object=CONFIG)
