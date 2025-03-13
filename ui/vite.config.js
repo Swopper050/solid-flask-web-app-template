2f20d4720b9c41f82afa6f0983c176b9eefb4a4d
Swopper050 <bram@witoos.nl>
2025-03-13 10:50:50 +0100

    Upgrade to daisyui v5 and tailwindcss v4

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     solidPlugin(),
   ],
   build: {
