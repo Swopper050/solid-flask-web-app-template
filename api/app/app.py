354f15ec1266038f17332af43a9e1159ee3e9872
Swopper050 <bram@witoos.nl>
2025-03-14 09:34:54 +0100

    🪵 Added logging for api

--- api/app/app.py
+++ api/app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import logging
+
+from flask import Flask, request
 
 from app.config import DevConfig, ProdConfig, TestConfig
 from app.errors import APIError
@@ -17,6 +19,31 @@ def create_app(config_object: DevConfig | ProdConfig | TestConfig = ProdConfig()
 
     @api.errorhandler(APIError)
     def handle_api_error(error):
+        app.logger.info(
+            "%s %s - error: %s [%s %s]",
+            request.method,
+            request.path,
+            error.status,
+            error.code.value,
+            error.code.name,
+        )
         return error.to_response()
+
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(logging.DEBUG if app.config["DEBUG"] else logging.INFO)
+
+    @app.after_request
+    def logging_after_request(response):
+        app.logger.info(
+            "%s %s [%s] %s %s %s",
+            request.method,
+            request.path,
+            response.status,
+            request.referrer,
+            request.remote_addr,
+            request.user_agent,
+        )
+        return response
 
     return app
