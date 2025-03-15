98a9361a98c111b1debee76f0efa6ff4b8834435
Swopper050 <bram@witoos.nl>
2025-03-15 23:41:32 +0100

    Cleanup

--- ui/cypress.config.js
+++ ui/cypress.config.js
@@ -4,8 +4,5 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
     experimentalStudio: true,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
