406a3923b9731bd938b0635a04f2c7aa9514d0ba
Swopper050 <bram@witoos.nl>
2025-04-29 09:34:59 +0200

    ✔️ Added celery background tasks for mailing

--- api/app/app.py
+++ api/app/app.py
@@ -1,6 +1,7 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
+from celery import Celery, Task
 from flask import Flask, request
 
 from app.commands import register_commands
@@ -18,6 +19,7 @@ def create_app(config_object: DevConfig | ProdConfig | TestConfig = ProdConfig()
     api.init_app(app)
     migrate.init_app(app, db)
     mail.init_app(app)
+    init_celery_app(app)
 
     register_commands(app)
 
@@ -76,3 +78,16 @@ def create_app(config_object: DevConfig | ProdConfig | TestConfig = ProdConfig()
         app.logger.addHandler(file_handler)
 
     return app
+
+
+def init_celery_app(app) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
