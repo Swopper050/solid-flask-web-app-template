2423b6c9934032322f85d1b42b06d326ccfd4af1
Swopper050 <bram@witoos.nl>
2026-03-22 20:43:27 +0100

    Fixed cypress tests

--- ui/cypress/e2e/admin.cy.js
+++ ui/cypress/e2e/admin.cy.js
@@ -6,123 +6,128 @@ describe('admin', () => {
   it('Create new user', () => {
     cy.visit('/login')
 
-    cy.wait(100);
+    cy.wait(100)
 
-    cy.get('#email').clear();
-    cy.get('#email').type('admin@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('admin');
-    cy.get('button[type="submit"]').click();
+    cy.get('#email').clear()
+    cy.get('#email').type('admin@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('admin')
+    cy.get('button[type="submit"]').click()
 
-    cy.wait(100);
+    cy.wait(100)
 
-    cy.get('.text').click();
-    cy.get('p.text-success').click();
-    cy.get('thead > tr > .text-end > .btn').click();
+    cy.get('.text').click()
+    cy.get('p.text-success').click()
+    cy.get('thead > tr > .text-end > .btn').click()
 
-    cy.wait(100);
+    cy.wait(100)
 
-    cy.get('#email').clear();
-    cy.get('#email').type('test@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('Testing1!');
-    cy.get('.modal-action > .btn').click();
+    cy.get('#email').clear()
+    cy.get('#email').type('test@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('Testing1!')
+    cy.get('.modal-action > .btn').click()
 
-    cy.wait(100);
+    cy.wait(100)
 
-    cy.get(':nth-child(3) > summary.btn').click();
-    cy.wait(50);
-    cy.get('.menu > :nth-child(3) > .btn').click();
-    cy.wait(50);
-    cy.visit('/login');
-    cy.wait(50);
+    cy.get('.modal-box .btn-circle').click()
 
-    cy.get('#email').clear();
-    cy.get('#email').type('test@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('Testing1!');
-    cy.get('button[type="submit"]').click();
+    cy.get(':nth-child(3) > summary.btn').click()
+    cy.get('.fa-arrow-right-from-bracket').click({ force: true })
+    cy.wait(50)
+    cy.visit('/login')
+    cy.wait(50)
 
-    cy.wait(50);
+    cy.get('#email').clear()
+    cy.get('#email').type('test@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('Testing1!')
+    cy.get('button[type="submit"]').click()
+
+    cy.wait(50)
 
     cy.url().should('include', '/home')
   })
 
-  it('Create another admin', function() {
-    cy.visit('/login');
-    cy.get('#email').clear();
-    cy.get('#email').type('admin@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('admin');
-    cy.get('button[type="submit"]').click();
-    cy.get('.text').click();
-    cy.get('p.text-success').click();
-    cy.get('thead > tr > .text-end').click();
-    cy.get('.modal-box').click();
-    cy.get('#email').clear();
-    cy.get('#email').type('admin2@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('Testing1@');
-    cy.get('.checkbox').check();
-    cy.get('.modal-action > .btn').click();
-    cy.get('.text').click();
-    cy.get('.menu > :nth-child(3) > .btn').click();
-    cy.visit('/login');
-    cy.get('#email').clear();
-    cy.get('#email').type('admin2@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('Testing1@');
-    cy.get('button[type="submit"]').click();
-    cy.get('.text').click();
-    cy.get(':nth-child(2) > .btn > .fa-solid').click();
+  it('Create another admin', function () {
+    cy.visit('/login')
+    cy.get('#email').clear()
+    cy.get('#email').type('admin@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('admin')
+    cy.get('button[type="submit"]').click()
+    cy.get('.text').click()
+    cy.get('p.text-success').click()
+    cy.get('thead > tr > .text-end').click()
+    cy.get('.modal-box').click()
+    cy.get('#email').clear()
+    cy.get('#email').type('admin2@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('Testing1@')
+    cy.get('.checkbox').check()
+    cy.get('.modal-action > .btn').click()
+    cy.get('.text').click()
+    cy.get('.fa-arrow-right-from-bracket').click({ force: true })
+
+    cy.visit('/login')
+    cy.get('#email').clear()
+    cy.get('#email').type('admin2@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('Testing1@')
+    cy.get('button[type="submit"]').click()
+    cy.get('.text').click()
+    cy.get(':nth-child(2) > .btn > .fa-solid').click()
 
     cy.url().should('include', '/admin-panel')
-  });
+  })
 
-  it('Delete user', function() {
-    cy.visit('/login');
-    cy.get('#email').clear();
-    cy.get('#email').type('admin@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('admin');
-    cy.get('button[type="submit"]').click();
-    cy.get('.text').click();
-    cy.get('p.text-success').click();
-    cy.get('thead > tr > .text-end > .btn > .fa-solid').click();
-    cy.get('#email').clear();
-    cy.get('#email').type('test@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('Testing1!');
-    cy.get('.modal-action > .btn').click();
-    cy.get('.text').click();
-    cy.get('.menu > :nth-child(3) > .btn').click();
-    cy.visit('/login');
-    cy.get('#email').clear();
-    cy.get('#email').type('test@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('Testing1!');
-    cy.get('button[type="submit"]').click();
-    cy.get('.text').click();
-    cy.get('[open=""] > .menu > :nth-child(2) > .btn').click();
-    cy.visit('/login');
-    cy.get('#email').clear();
-    cy.get('#email').type('admin@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('admin');
-    cy.get('button[type="submit"]').click();
-    cy.get('.text').click();
-    cy.get('p.text-success').click();
-    cy.get(':nth-child(2) > .text-end > .btn > .fa-solid').click();
-    cy.get('.btn-error').click();
-    cy.get('.text').click();
-    cy.get('.menu > :nth-child(3) > .btn > .fa-solid').click();
-    cy.visit('/login');
-    cy.get('#email').clear();
-    cy.get('#email').type('test@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('Testing1!');
-    cy.get('button[type="submit"]').click();
+  it('Delete user', function () {
+    cy.visit('/login')
+    cy.get('#email').clear()
+    cy.get('#email').type('admin@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('admin')
+    cy.get('button[type="submit"]').click()
+    cy.get('.text').click()
+    cy.get('p.text-success').click()
+    cy.get('thead > tr > .text-end > .btn > .fa-solid').click()
+    cy.get('#email').clear()
+    cy.get('#email').type('test@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('Testing1!')
+    cy.get('.modal-action > .btn').click()
+    cy.get('.text').click()
+    cy.get('.fa-arrow-right-from-bracket').click({ force: true })
+    cy.visit('/login')
+    cy.get('#email').clear()
+    cy.get('#email').type('test@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('Testing1!')
+    cy.get('button[type="submit"]').click()
+    cy.get('.text').click()
+    cy.get('[open=""] > .menu > :nth-child(2) > .btn').click()
+    cy.visit('/login')
+    cy.get('#email').clear()
+    cy.get('#email').type('admin@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('admin')
+    cy.get('button[type="submit"]').click()
+    cy.get('.text').click()
+    cy.get('p.text-success').click()
+    cy.get(':nth-child(2) > .text-end > .btn > .fa-solid').click()
+    cy.get('.btn-error').click()
+    cy.get('.text').click()
+    cy.get('.fa-arrow-right-from-bracket').click({ force: true })
+    cy.visit('/login')
+    cy.get('#email').clear()
+    cy.get('#email').type('test@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('Testing1!')
+    cy.get('button[type="submit"]').click()
 
-    cy.get('.alert span').should('contain.text', 'Could not login with the given email and password');
-  });
+    cy.get('.alert span').should(
+      'contain.text',
+      'Could not login with the given email and password'
+    )
+  })
 })
--- ui/cypress/e2e/authentication.cy.js
+++ ui/cypress/e2e/authentication.cy.js
@@ -6,141 +6,150 @@ describe('authentication', () => {
   it('Login as admin', () => {
     cy.visit('/login')
 
-    cy.get('#email').clear();
-    cy.get('#email').type('admin@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('admin');
-    cy.get('button[type="submit"]').click();
+    cy.get('#email').clear()
+    cy.get('#email').type('admin@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('admin')
+    cy.get('button[type="submit"]').click()
 
     cy.url().should('include', '/home')
   })
 
-  it('Register new user', function() {
-    cy.visit('/register');
+  it('Register new user', function () {
+    cy.visit('/register')
 
-    cy.get('#name').clear();
-    cy.get('#name').type('Test User');
-    cy.get('#email').clear();
-    cy.get('#email').type('test@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('Testing1!');
-    cy.get('#checkPassword').clear();
-    cy.get('#checkPassword').type('Testing1!');
-    cy.get('button[type="submit"]').click();
-
-    cy.url().should('include', '/home')
-  });
-
-  it('Logout', function() {
-    cy.visit('/login');
-    cy.get('#email').clear();
-    cy.get('#email').type('admin@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('admin');
-    cy.get('button[type="submit"]').click();
+    cy.get('#name').clear()
+    cy.get('#name').type('Test User')
+    cy.get('#email').clear()
+    cy.get('#email').type('test@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('Testing1!')
+    cy.get('#checkPassword').clear()
+    cy.get('#checkPassword').type('Testing1!')
+    cy.get('button[type="submit"]').click()
 
     cy.url().should('include', '/home')
-    cy.wait(200);
+  })
 
-    cy.get(':nth-child(3) > summary.btn').click();
-    cy.get('.menu > :nth-child(3) > .btn').click();
+  it('Logout', function () {
+    cy.visit('/login')
+    cy.get('#email').clear()
+    cy.get('#email').type('admin@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('admin')
+    cy.get('button[type="submit"]').click()
+
+    cy.url().should('include', '/home')
+    cy.wait(200)
+
+    cy.get(':nth-child(3) > summary.btn').click()
+    cy.get('.menu > :nth-child(3) > .btn').click()
 
     cy.url().should('not.include', '/home')
-  });
+  })
 
-  it('Forgot password', function() {
-    cy.visit('/login');
-    cy.get('a[href="/forgot-password"]').click();
-    cy.get('#email').clear();
-    cy.get('#email').type('admin@test.nl');
-    cy.get('button[type="submit"]').click();
-  });
+  it('Forgot password', function () {
+    cy.visit('/login')
+    cy.get('a[href="/forgot-password"]').click()
+    cy.get('#email').clear()
+    cy.get('#email').type('admin@test.nl')
+    cy.get('form button[type="submit"]').first().click();
+  })
 
-  it('Login failed with incorrect password', function() {
-    cy.visit('/login');
-    cy.get('#email').clear();
-    cy.get('#email').type('admin@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('test');
-    cy.get('button[type="submit"]').click();
+  it('Login failed with incorrect password', function () {
+    cy.visit('/login')
+    cy.get('#email').clear()
+    cy.get('#email').type('admin@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('test')
+    cy.get('button[type="submit"]').click()
 
-    cy.get('.alert span').should('contain.text', 'Could not login with the given email and password');
-  });
+    cy.get('.alert span').should(
+      'contain.text',
+      'Could not login with the given email and password'
+    )
+  })
 
-  it('Register failed email already exists', function() {
-    cy.visit('/register');
-    cy.get('#name').clear();
-    cy.get('#name').type('Admin User');
-    cy.get('#email').clear();
-    cy.get('#email').type('admin@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('Testing1!');
-    cy.get('#checkPassword').clear();
-    cy.get('#checkPassword').type('Testing1!');
-    cy.get('button[type="submit"]').click();
+  it('Register failed email already exists', function () {
+    cy.visit('/register')
+    cy.get('#name').clear()
+    cy.get('#name').type('Admin User')
+    cy.get('#email').clear()
+    cy.get('#email').type('admin@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('Testing1!')
+    cy.get('#checkPassword').clear()
+    cy.get('#checkPassword').type('Testing1!')
+    cy.get('button[type="submit"]').click()
 
-    cy.get('.alert span').should('contain.text', 'An account with this email already exists');
+    cy.get('.alert span').should(
+      'contain.text',
+      'An account with this email already exists'
+    )
     cy.url().should('not.include', '/home')
-  });
+  })
 
-  it('Change password', function() {
-    cy.visit('/login');
-    cy.get('#email').clear();
-    cy.get('#email').type('admin@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('admin');
-    cy.get('button[type="submit"]').click();
-
-    cy.url().should('include', '/home')
-
-    cy.get('.text').click();
-    cy.get('[open=""] > .menu > :nth-child(1) > .btn').click();
-    cy.get(':nth-child(2) > .text-end > .tooltip > .btn > .fa-solid').click();
-    cy.get('#currentPassword').clear();
-    cy.get('#currentPassword').type('admin');
-    cy.get('#newPassword').clear();
-    cy.get('#newPassword').type('Testing1@');
-    cy.get('#confirmNewPassword').clear();
-    cy.get('#confirmNewPassword').type('Testing1@{enter}');
-
-    cy.get('.text').click();
-    cy.get('.menu > :nth-child(3) > .btn').click();
-    cy.visit('/login');
-
-    cy.get('#email').clear();
-    cy.get('#email').type('admin@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('Testing1@');
-    cy.get('button[type="submit"]').click();
-
-    cy.url().should('include', '/home')
-  });
-
-  it('Delete own account', function() {
-    cy.visit('/register');
-    cy.get('#name').clear();
-    cy.get('#name').type('Test User');
-    cy.get('#email').clear();
-    cy.get('#email').type('test@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('Testing1!');
-    cy.get('#checkPassword').clear();
-    cy.get('#checkPassword').type('Testing1!');
-    cy.get('button[type="submit"]').click();
+  it('Change password', function () {
+    cy.visit('/login')
+    cy.get('#email').clear()
+    cy.get('#email').type('admin@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('admin')
+    cy.get('button[type="submit"]').click()
 
     cy.url().should('include', '/home')
 
-    cy.get('.text').click();
-    cy.get('[open=""] > .menu > :nth-child(1) > .btn').click();
-    cy.get('div.mt-4 > .btn').click();
-    cy.get('.modal-action > .btn-error').click();
-    cy.visit('/login');
-    cy.get('#email').clear();
-    cy.get('#email').type('test@test.nl');
-    cy.get('#password').clear();
-    cy.get('#password').type('Testing1!');
-    cy.get('button[type="submit"]').click();
+    cy.get('.text').click()
+    cy.get('[open=""] > .menu > :nth-child(1) > .btn').click()
+    cy.get(':nth-child(2) > .text-end > .tooltip > .btn > .fa-solid').click()
+    cy.get('#currentPassword').clear()
+    cy.get('#currentPassword').type('admin')
+    cy.get('#newPassword').clear()
+    cy.get('#newPassword').type('Testing1@')
+    cy.get('#confirmNewPassword').clear()
+    cy.get('#confirmNewPassword').type('Testing1@{enter}')
 
-    cy.get('.alert span').should('contain.text', 'Could not login with the given email and password');
-  });
+    cy.get('.text').click()
+    cy.get('.fa-arrow-right-from-bracket').click({ force: true })
+    cy.visit('/login')
+
+    cy.get('#email').clear()
+    cy.get('#email').type('admin@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('Testing1@')
+    cy.get('button[type="submit"]').click()
+
+    cy.url().should('include', '/home')
+  })
+
+  it('Delete own account', function () {
+    cy.visit('/register')
+    cy.get('#name').clear()
+    cy.get('#name').type('Test User')
+    cy.get('#email').clear()
+    cy.get('#email').type('test@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('Testing1!')
+    cy.get('#checkPassword').clear()
+    cy.get('#checkPassword').type('Testing1!')
+    cy.get('button[type="submit"]').click()
+
+    cy.url().should('include', '/home')
+
+    cy.get('.text').click()
+    cy.get('[open=""] > .menu > :nth-child(1) > .btn').click()
+    cy.get('div.mt-4 > .btn').click()
+    cy.get('.modal-action > .btn-error').click()
+    cy.visit('/login')
+    cy.get('#email').clear()
+    cy.get('#email').type('test@test.nl')
+    cy.get('#password').clear()
+    cy.get('#password').type('Testing1!')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.alert span').should(
+      'contain.text',
+      'Could not login with the given email and password'
+    )
+  })
 })
